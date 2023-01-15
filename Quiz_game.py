67a84f41ac5e54dfaf549c69aa654f6c5262c10a
Zineb-MOUHI <82595945+Zineb-MOUHI@users.noreply.github.com>
2023-01-15 20:55:30 +0100

    Create Quiz_game.py
    Adding Quiz_game.py

---
+++ Quiz_game.py
@@ -0,0 +1,42 @@
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play ? ")
+print (playing)
+if playing.lower() != "yes":
+    quit()
+print("let's play then ! :) ")
+score = 0
+answer = input("what does CPU stand for ? ")
+if answer.lower() == "central processing unit":
+    print('correct! ')
+    score += 1
+else:
+    print("Incorrect!")
+
+    
+answer = input("What does GPU stand for ?  ")
+if answer.lower() == "graphics processing unit":
+    print('correct! ')
+    score += 1
+else:
+    print("Incorrect!")
+
+   
+answer = input("What does RAM stand for ? ")
+if answer.lower() == "random access memory":
+    print('correct! ')
+    score += 1
+else:
+    print("Incorrect!")
+
+   
+answer = input("what does PSU stand for ? ")
+if answer.lower() == "power supply":
+    print('correct! ')
+    score += 1
+else:
+    print("Incorrect!")
+
+print("Yous got " + str(score) + " questions correct!")
+print("Yous got " + str((score / 4) * 100) + "%.")
+
